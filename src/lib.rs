1c78d5abd840e307af412dcbf6aa4933df497242
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2021-02-10 14:18:59 -0500

    Added panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate console_error_panic_hook;
+
 use wasm_bindgen::prelude::*;
 use crate::app::App;
 
@@ -9,5 +11,7 @@ mod utils;
 
 #[wasm_bindgen(start)]
 pub async fn main_js() {
+    console_error_panic_hook::set_once();
+
     dominator::append_dom(&dominator::get_id("app"), App::render().await);
 }
